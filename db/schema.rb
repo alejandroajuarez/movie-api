8ed1e8a8bc8cc876b7ea2ceee6f50b916025448b
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-26 12:32:37 -0700

    add Movies model and seeded 5 movies associated with 5 actors in actors model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_17_031638) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_191820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_17_031638) do
     t.string "first_name"
     t.string "last_name"
     t.string "known_for"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "movie_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,22 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-10.times do
-  Actor.create!(
-    first_name: FFaker::Name.first_name,
-    last_name: FFaker::Name.last_name,
-    known_for: FFaker::Movie.title
-  )
-end
+# 10.times do
+#   Actor.create!(
+#     first_name: FFaker::Name.first_name,
+#     last_name: FFaker::Name.last_name,
+#     known_for: FFaker::Movie.title
+#   )
+# end
 
-puts "Seeded 10 actors"
+# puts "Seeded 10 actors"
+
+# movie1 = Movie.create(title: "Intersteller", year: 2014, plot: "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home.")
+
+# movie2 = Movie.create(title: "Forrest Gump", year: 1994, plot: "Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny (Robin Wright)")
+
+# movie3 = Movie.create(title: "The Wolf of Wall Street", year: 2013, plot: "In 1987, Jordan Belfort takes an entry-level job at a Wall Street brokerage firm. By the early 1990s, while still in his 20s, Belfort founds his own firm, Stratton Oakmont. Together with his trusted lieutenant and a merry band of brokers, Belfort makes a huge fortune by defrauding wealthy investors out of millions. However, while Belfort and his cronies partake in a hedonistic brew of sex, drugs and thrills, the SEC and the FBI close in on his empire of excess.")
+
+# movie4 = Movie.create(title: "Black Widow", year: 2021, plot: "Natasha Romanoff, aka Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy, and the broken relationships left in her wake long before she became an Avenger.")
+
+# movie5 = Movie.create(title: "Man of Steel", year: 2013, plot: "With the imminent destruction of Krypton, their home planet, Jor-El (Russell Crowe) and his wife seek to preserve their race by sending their infant son to Earth. The child's spacecraft lands at the farm of Jonathan (Kevin Costner) and Martha (Diane Lane) Kent, who name him Clark and raise him as their own son. Though his extraordinary abilities have led to the adult Clark (Henry Cavill) living on the fringe of society, he finds he must become a hero to save those he loves from a dire threat.")
