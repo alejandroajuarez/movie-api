ef417b2d167330602e926d25f1cc713f303ccff0
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-09 16:35:57 -0700

    add movie model to actor controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,36 +1,52 @@
 class ActorsController < ApplicationController
   def index
-    @actors = Actor.all
-    render :index
+    @actors = Actor.includes(:movie) 
+    render json: @actors, include: { movie: { only: [:title, :year, :plot, :image_url] } }
   end
 
   def show
-    id_find = params[:id]
-    @actor = Actor.find_by(id: id_find)
-    render :show
+    @actor = Actor.includes(:movie).find_by(id: params[:id])
+    if @actor
+      render json: @actor, include: { movie: { only: [:title, :year, :plot, :image_url] } }
+    else
+      render json: { error: "Actor not found" }, status: :not_found
+    end
   end
 
   def create
     @actor = Actor.new(
       first_name: params[:input_fname],
       last_name:  params[:input_lname],
-      known_for:  params[:input_known_for]
+      known_for:  params[:input_known_for],
+      movie_id:   params[:movie_id]
     )
-    @actor.save
-    render :show
+
+    if @actor.save
+      render json: @actor, include: { movie: { only: [:title, :year, :plot, :image_url] } }
+    else
+      render json: { errors: @actor.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
     @actor = Actor.find_by(id: params[:id])
-    @actor.first_name = params[:first_name]
-    @actor.last_name = params[:last_name]
-    @actor.known_for = params[:known_for]
-    render :show
+    if @actor
+      @actor.update(
+        first_name: params[:first_name] ||  actor.first_name,
+        last_name:  params[:last_name]  ||  actor.last_name,
+        known_for:  params[:known_for]  ||  actor.known_for,
+        movie_id:   params[:movie_id]   ||  actor.movie_id
+      )
+      render json: @actor, include: { movie: { only: [:title, :year, :plot, :image_url] } }
+    else
+      render json: { error: "Actor not found" }, status: :not_found
+    end
   end
 
   def destroy
     actor = Actor.find_by(id: params[:id])
     actor.destroy
-    render json: { message: "You have deleted this actor field!" }
+    actor.destroy
+    render json: { message: "You have deleted this actor!" }
   end
 end
