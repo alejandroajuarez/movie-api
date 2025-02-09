078078b2e794030ab532a010b60d1b71c4026476
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-09 00:32:49 -0700

    add image_url to Movies rails api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_26_191820) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_072726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,5 +29,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_26_191820) do
     t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image_url"
   end
 end
